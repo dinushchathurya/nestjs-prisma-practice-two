3faf01d10fe8ef97d175578a16c531e4029e3b88
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-27 20:43:40 +0530

    feat: Handle unique constraint error

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,10 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const prisma_client_exception_filter_1 = require("./prisma-client-exception.filter");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.useGlobalInterceptors(new common_1.ClassSerializerInterceptor(app.get(core_1.Reflector)));
+    const { httpAdapter } = app.get(core_1.HttpAdapterHost);
+    app.useGlobalFilters(new prisma_client_exception_filter_1.PrismaClientExceptionFilter(httpAdapter));
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory, Reflector, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, ClassSerializerInterceptor  } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter))
   await app.listen(3000);
 }
 bootstrap();
